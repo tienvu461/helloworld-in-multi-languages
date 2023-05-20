7bc0db2b9e98bd374d08a5ad37909e62a810f536
tienvu461 <tienvu461@gmail.com>
2023-05-20 21:41:01 +0700

    add: healthcheck data

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs');
 
 // Constants
 const PORT = 3000;
@@ -9,7 +10,12 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    fs.readFile('health.json', (e, data) => {
+        if (e) throw e;
+        let health = JSON.parse(data);
+        res.setHeader('content-type', 'application/json');
+        res.send(health);
+    });
 });
 
 app.listen(PORT, HOST);
